84eba678a297ab56253c67330d423889cc2f9f28
iZarrios <zarriose@gmail.com>
2023-02-08 12:42:46 +0200

    Add ping handler

---
+++ controllers/ping.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Ping(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"code":    http.StatusOK,
+		"message": "pong",
+	})
+
+}
