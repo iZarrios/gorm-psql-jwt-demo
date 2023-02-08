644bf0c9238cdedff733abeb92b747d4bb856642
iZarrios <zarriose@gmail.com>
2023-02-08 16:42:34 +0200

    update user model

--- models/user.go
+++ models/user.go
@@ -1,6 +1,18 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
 
+// UserDB
 type User struct {
+	Id       uint    `gorm:"primary key;autoIncrement" json:"_id"`
+	UserName *string `gorm:"not null" json:"user_name"`
+	Password *string `gorm:"not null" json:"password"`
+	Email    *string `gorm:"not null;unique" json:"email"`
+}
 
+func MigrateUser(db *gorm.DB) error {
+	err := db.AutoMigrate(&User{})
+	return err
 }
