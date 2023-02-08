80435de87e3fbbb02642241043144d03c4db2ff6
iZarrios <zarriose@gmail.com>
2023-02-08 23:07:12 +0200

    update user model

--- models/user.go
+++ models/user.go
@@ -8,14 +8,20 @@ import (
 
 // UserDB
 type User struct {
-	// gorm.Model
-	Id        uint   `gorm:"primary key;autoIncrement" json:"_id"`
-	UserName  string `gorm:"not null" json:"user_name"`
-	Password  string `gorm:"not null" json:"password"`
-	Email     string `gorm:"not null;unique" json:"email"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Id        uint           `gorm:"primary key;autoIncrement" json:"id"`
+	UserName  string         `gorm:"not null" json:"username"`
+	Password  string         `gorm:"not null" json:"password"`
+	Email     string         `gorm:"not null;unique" json:"email"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+}
+
+type UserResponse struct {
+	Id       uint   `json:"id"`
+	UserName string `json:"username"`
+	Password string ` json:"password"`
+	Email    string `json:"email"`
 }
 
 func MigrateUser(db *gorm.DB) error {
